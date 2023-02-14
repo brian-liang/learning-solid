4d5f28138a3ff738732fd44f1da8ff6bd127710d
brian-liang <brian.j.liang@gmail.com>
2023-02-14 13:56:05 -0700

    FIrst Commit

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+import type { Component } from 'solid-js';
+
+import logo from './logo.svg';
+import styles from './App.module.css';
+import HelloWorld from './components/HelloWorld';
+
+const App: Component = () => {
+  return (
+    <div>
+        <h1>Welcome</h1>
+        <HelloWorld/>
+    </div>
+  );
+};
+
+export default App;
